732663e85e25acf717fc06e62facc013655e1ffd
shinealove <43578700+shinealove@users.noreply.github.com>
2019-07-21 23:06:46 +0800

    MyTestBean ch05

---
+++ demo/src/main/java/com/example/demo/myTestBean/MyTestBean.java
@@ -0,0 +1,13 @@
+package com.example.demo.myTestBean;
+
+public class MyTestBean {
+	private String testStr = "testStr";
+
+	public String getTestStr() {
+		return testStr;
+	}
+
+	public void setTestStr(String testStr) {
+		this.testStr = testStr;
+	}
+}
--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
 import com.example.demo.customtag.User;
+import com.example.demo.myTestBean.MyTestBean;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -14,6 +18,13 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testSimpleLoad(){
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
+		MyTestBean bean = (MyTestBean) bf.getBean("myTestBean");
+
 	}
 
 	@Test
